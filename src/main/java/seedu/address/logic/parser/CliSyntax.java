56a37f8a4297bf73ae3015c1878218f10a82decf
DanKhoo <>
2018-10-03 16:01:19 +0800

    CliSyntax.java: Fix PRFIX_MANAGER

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,7 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_DEPARTMENT = new Prefix("d/");
-    public static final Prefix PREFIX_MANAGER = new Prefix("m");
+    public static final Prefix PREFIX_MANAGER = new Prefix("m/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
 }
